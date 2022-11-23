9d559d19f98c23b7cda1ee673b5dc0750c86acf8
Ruslan <RMingalievF@icloud.com>
2022-11-23 23:29:55 +0300

    add Outlet to contact detail view

---
+++ ContactList/ContactDetailsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ContactDetailsViewController.swift
+//  ContactList
+//
+//  Created by Руслан Мингалиев on 23.11.2022.
+//
+
+import UIKit
+
+class ContactDetailsViewController: UIViewController {
+
+    
+    @IBOutlet weak var contactPhotoImageView: UIImageView!
+    @IBOutlet weak var phoneNumberLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
