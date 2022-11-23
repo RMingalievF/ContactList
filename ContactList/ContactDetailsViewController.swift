21c869987ad01741cb7e820d4370da968c1a9ab2
Ruslan <RMingalievF@icloud.com>
2022-11-23 23:41:41 +0300

    complete contact list without random

--- ContactList/ContactDetailsViewController.swift
+++ ContactList/ContactDetailsViewController.swift
@@ -14,22 +14,14 @@ class ContactDetailsViewController: UIViewController {
     @IBOutlet weak var phoneNumberLabel: UILabel!
     @IBOutlet weak var emailLabel: UILabel!
     
+    var contact: Contact!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        phoneNumberLabel.text = contact.phoneNumber
+        emailLabel.text = contact.eMail
+        navigationItem.title = contact.contsctName
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
